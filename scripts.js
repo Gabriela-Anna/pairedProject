631e276ff334dda38441eef1c2274b8b2b96cf93
Gabriela Parada <gabrielaparada@Gabrielas-MacBook-Pro.local>
2020-04-14 18:08:08 -0400

    js api call started

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
+//create name space app
 const spaceApp = {}
 
+//links for api calls
 spaceApp.marsUrl = 'https://images-api.nasa.gov/search?media_type=image&q=mars'
 
 spaceApp.venusUrl = 'https://images-api.nasa.gov/search?media_type=image&q=venus'
@@ -18,3 +20,44 @@ spaceApp.neptuneUrl = 'https://images-api.nasa.gov/search?media_type=image&q=nep
 
 spaceApp.plutoUrl = 'https://images-api.nasa.gov/search?media_type=image&q=pluto'
 
+spaceApp.apiKey = '8iRdpFhDSRdRgbOBPn1KDTUQhkZBSCqcXG6cPBhk'
+
+spaceApp.getMars = function () {
+    console.log('sending request')
+    $.ajax({
+        url: spaceApp.marsUrl,
+        method: 'GET',
+        dataType: 'json',
+    }).then(function (result) {
+        const images = result.collection.items
+        images.forEach((image)=>{
+            console.log(image)
+        })
+        // console.log(result)
+        // result.forEach((res) => {
+        //     console.log(res)  
+            
+        // });
+        // console.log(result.collection.items[0].links[0].href)
+        // $('#mars').append(`<img src="${image}">`)
+    });
+};
+
+spaceApp.setUpEventListeners = function (){
+        $('#mars').on('click', function (e){
+            e.preventDefault()
+            spaceApp.getMars()
+            console.log("mars!!!")
+        })
+    
+}
+
+
+spaceApp.init = () => {
+    spaceApp.setUpEventListeners()
+
+}
+
+$(document).ready(function() { 
+    spaceApp.init()
+  });
