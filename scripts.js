f591465ca2d23407dc2e88c988c151eadd3ef284
Gabriela Parada <gabrielaparada@Gabrielas-MacBook-Pro.local>
2020-04-17 22:40:16 -0400

    commented code

--- scripts.js
+++ scripts.js
@@ -1,37 +1,28 @@
 //create name space app
-// console.log(lodash)
 const spaceApp = {}
 
 
-spaceApp.apiKey = '8iRdpFhDSRdRgbOBPn1KDTUQhkZBSCqcXG6cPBhk'
-
-
+//ajax call
 spaceApp.getPlanets = function (query) {
-    console.log('sending request')
     $.ajax({
         url: `https://images-api.nasa.gov/search?media_type=image&q=${query}`,
         method: 'GET',
         dataType: 'json',
     }).then(function (result) {
-        console.log(result)
         spaceApp.images = Array.from(result.collection.items)
         spaceApp.images.forEach((image) => {
+            //displaying planets matching user input
             if (!image.data[0].keywords.includes('Mars Celebration')) {
-                // do the displaying stuff in here 
-                // console.log(image.links[0].href);
                 $('.display-images').append(`
-
                     <div class="display-container">
                     <h2 class="heading-planets">${image.data[0].title}</h2>
                     <img class='${query}-images images' src="${image.links[0].href}" alt="${query}">
                     </div>
-                    `);
+                `);
             }
         });
     });
 }
-
-
 
 spaceApp.setUpEventListeners = function () {
     $('.planet-container').on('click', function (e) {
@@ -50,19 +41,13 @@ spaceApp.setUpEventListeners = function () {
     });
 }
 
-    // $('.close').on('click', function (e) {
-    //     e.preventDefault();
-    //     $('.display-images').empty();
-    //     $('.planets').css('display', 'flex');
-    //     $('.close').css('display', 'none');
-    // });
-
-
+//init
 spaceApp.init = () => {
     spaceApp.setUpEventListeners()
 
 }
 
+//document ready
 $(document).ready(function() { 
     spaceApp.init()
 });
