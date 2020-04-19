454d35a327165c68f78bd434d927ec6d82aaa6e4
Gabriela Parada <gabrielaparada@Gabrielas-MacBook-Pro.local>
2020-04-18 22:19:20 -0400

    added text under planets name on display

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,49 +10,34 @@ const typed = new Typed('#typed', {
     showCursor: false
 });
 
-// const typedTwo = new Typed('#type', {
-//     strings: [
-//         "Click on image to enlarge",
-//     ],
-//     typeSpeed: 30,
-//     backSpeed: 30,
-//     backDelay: 700,
-//     smartBackspace: false, // Default value
-//     loop: false,
-//     showCursor: false
-// });
-
-
 
 
 //create name space app
 const spaceApp = {}
 
-
-//ajax call
 spaceApp.getPlanets = function (query) {
     $.ajax({
         url: `https://images-api.nasa.gov/search?media_type=image&q=${query}`,
         method: 'GET',
         dataType: 'json',
-        beforeSend: function(){
+        beforeSend: function () {
             $('#loader').removeClass('hidden')
-        }, success: function(result){
-        spaceApp.images = Array.from(result.collection.items)
-        spaceApp.images.forEach((image) => {
-            //displaying planets matching user input
-            if (!image.data[0].keywords.includes('Mars Celebration')) {
-                $('.display-images').append(`
+        }, success: function (result) {
+            spaceApp.images = Array.from(result.collection.items)
+            spaceApp.images.forEach((image) => {
+                //displaying planets matching user input
+                if (!image?.data[0]?.keywords?.includes('Mars Celebration')) {
+                    $('.display-images').append(`
                     <div class="display-container">
                     <h2 class="heading-planets">${image.data[0].title}</h2>
                     <img class='${query}-images images' src="${image.links[0].href}" alt="${query}" data-action="zoom">
                     </div>
                 `);
-            }
-        });
-    }, complete: function(){
-        $('#loader').addClass('hidden')
-    }
+                }
+            });
+        }, complete: function () {
+            $('#loader').addClass('hidden')
+        }
     });
 }
 
@@ -68,12 +53,10 @@ spaceApp.setUpEventListeners = function () {
         $('.type').css('display', 'none')
         $('.space-title').css('display', 'none')
         if (planetsId === 'earth observation') {
-            $('.space-subheading').append(`
-                <h2 class="space-subheading">Earth</h2>
-                <p class="type">Click on image to enlarge</p>
-            `).css('height', '20vh').css('text-transform', "capitalize")
+            $('.space-subheading').append(`Earth
+            <p class="space-text">Click image to enlarge</p>`).css('height', '20vh').css('text-transform', "capitalize")
         } else {
-            $('.space-subheading').append(`${(planetsId)}`).css('height', '20vh').css('text-transform', "capitalize")
+            $('.space-subheading').append(`${(planetsId)} <p class="space-text">Click image to enlarge</p>`).css('height', '20vh').css('text-transform', "capitalize")
         }
     });
 
